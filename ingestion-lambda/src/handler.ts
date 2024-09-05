34fbde1b4441c1a5ad80a387bcc1900e5b276829
Pete F <pete.faulconbridge@theguardian.com>
2024-09-05 14:38:39 +0100

    Close sql connection in finally block

--- ingestion-lambda/src/handler.ts
+++ ingestion-lambda/src/handler.ts
@@ -17,70 +17,70 @@ export const main = async (event: SQSEvent): Promise<SQSBatchResponse> => {
 
 	const sql = await createDbConnection();
 
-	console.log(`Processing ${records.length} messages`);
+	try {
+		console.log(`Processing ${records.length} messages`);
 
-	const results = await Promise.all(
-		records.map(
-			async ({
-				messageId,
-				messageAttributes,
-				body,
-			}): Promise<OperationResult> => {
-				const sqsMessageId = messageId;
+		const results = await Promise.all(
+			records.map(
+				async ({
+					messageId,
+					messageAttributes,
+					body,
+				}): Promise<OperationResult> => {
+					const sqsMessageId = messageId;
 
-				try {
-					const fingerpostMessageId =
-						messageAttributes['Message-Id']?.stringValue;
+					try {
+						const fingerpostMessageId =
+							messageAttributes['Message-Id']?.stringValue;
 
-					if (!fingerpostMessageId) {
-						console.warn(
-							`Message ${sqsMessageId} is missing Message-Id attribute`,
+						if (!fingerpostMessageId) {
+							console.warn(
+								`Message ${sqsMessageId} is missing Message-Id attribute`,
+							);
+						}
+
+						// todo -- consider storing s3 object version in db
+						await s3Client.send(
+							new PutObjectCommand({
+								Bucket: BUCKET_NAME,
+								Key: `${fingerpostMessageId}.json`,
+								Body: body,
+							}),
 						);
-					}
 
-					// todo -- consider storing s3 object version in db
-					await s3Client.send(
-						new PutObjectCommand({
-							Bucket: BUCKET_NAME,
-							Key: `${fingerpostMessageId}.json`,
-							Body: body,
-						}),
-					);
-
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- seems like postgres.js requires this format? https://github.com/porsager/postgres/issues/587#issuecomment-1563262612
-					const snsMessageContent = JSON.parse(body);
-					await sql`
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- seems like postgres.js requires this format? https://github.com/porsager/postgres/issues/587#issuecomment-1563262612
+						const snsMessageContent = JSON.parse(body);
+						await sql`
 						INSERT INTO ${sql(tableName)}
 							(external_id, content)
 						VALUES
 							(${fingerpostMessageId ?? null}, ${snsMessageContent})
 						RETURNING id`.then((res) => {
-						if (res.length === 0) {
-							return 'failure';
-						}
-						return 'success';
-					});
-				} catch {
-					return Promise.resolve({
-						status: 'failure',
-						sqsMessageId,
-					});
-				}
+							if (res.length === 0) {
+								return 'failure';
+							}
+							return 'success';
+						});
+					} catch {
+						return Promise.resolve({
+							status: 'failure',
+							sqsMessageId,
+						});
+					}
 
-				return { sqsMessageId, status: 'success' };
-			},
-		),
-	);
+					return { sqsMessageId, status: 'success' };
+				},
+			),
+		);
+		const batchItemFailures = results
+			.filter(({ status }) => status === 'failure')
+			.map(({ sqsMessageId }) => ({ itemIdentifier: sqsMessageId }));
+		console.log(
+			`Processed ${records.length} messages with ${batchItemFailures.length} failures`,
+		);
 
-	await sql.end();
-
-	const batchItemFailures = results
-		.filter(({ status }) => status === 'failure')
-		.map(({ sqsMessageId }) => ({ itemIdentifier: sqsMessageId }));
-
-	console.log(
-		`Processed ${records.length} messages with ${batchItemFailures.length} failures`,
-	);
-
-	return { batchItemFailures };
+		return { batchItemFailures };
+	} finally {
+		await sql.end();
+	}
 };
