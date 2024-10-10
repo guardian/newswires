cc05d40939ccd758dedefe891380b6b6ccfa794e
Pete F <pete.faulconbridge@theguardian.com>
2024-10-10 17:21:30 +0100

    Add side nav, hide feed for mobile item view

--- newswires/client/src/Item.tsx
+++ newswires/client/src/Item.tsx
@@ -1,14 +1,18 @@
 import {
 	EuiButton,
-	EuiFlyout,
-	EuiFlyoutBody,
-	EuiFlyoutFooter,
+	EuiButtonIcon,
+	EuiCopy,
+	EuiFlexGroup,
+	EuiFlexItem,
+	EuiHorizontalRule,
 	EuiPageTemplate,
 	EuiSpacer,
+	EuiSplitPanel,
 	EuiTitle,
+	EuiToolTip,
 	useGeneratedHtmlId,
 } from '@elastic/eui';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { type WireData, WireDataSchema } from './sharedTypes';
 import { useSearch } from './useSearch';
 import { WireDetail } from './WireDetail';
@@ -18,6 +22,8 @@ export const Item = ({ id }: { id: string }) => {
 
 	const [itemData, setItemData] = useState<WireData | undefined>(undefined);
 	const [error, setError] = useState<string | undefined>(undefined);
+
+	const currentUrl = useMemo(() => window.location.href, []);
 
 	const pushedFlyoutTitleId = useGeneratedHtmlId({
 		prefix: 'pushedFlyoutTitle',
@@ -57,35 +63,83 @@ export const Item = ({ id }: { id: string }) => {
 	}, [id]);
 
 	return (
-		<EuiPageTemplate.Section>
+		<EuiSplitPanel.Outer>
 			{error && (
 				<EuiPageTemplate.EmptyPrompt>
 					<p>{error}</p>
 				</EuiPageTemplate.EmptyPrompt>
 			)}
 			{itemData && (
-				<EuiFlyout
-					type="push"
-					size="m"
-					onClose={() => handleDeselectItem()}
-					closeButtonProps={{
-						'aria-label': 'Close wire detail',
-						autoFocus: true,
-					}}
-					aria-labelledby={pushedFlyoutTitleId}
-				>
-					<EuiFlyoutBody>
-						<EuiTitle size="s">
-							<h2 id={pushedFlyoutTitleId}>{itemData.content.headline}</h2>
-						</EuiTitle>
+				<>
+					<EuiSplitPanel.Inner>
+						<EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
+							<EuiButtonIcon
+								iconType="arrowLeft"
+								onClick={handleDeselectItem}
+							/>
+							<EuiFlexGroup justifyContent="flexEnd" alignItems="center">
+								<EuiToolTip
+									position="top"
+									content={
+										<p>
+											Works on any kind of element &mdash; buttons, inputs, you
+											name it!
+										</p>
+									}
+								>
+									<EuiButtonIcon iconType={'launch'} size="s" />
+								</EuiToolTip>
+								<EuiButtonIcon iconType={'heart'} size="s" />
+								<CopyButton textToCopy={currentUrl} />
+							</EuiFlexGroup>
+						</EuiFlexGroup>
+						<EuiHorizontalRule margin="s" />
+						<EuiFlexGroup justifyContent="spaceBetween">
+							<EuiFlexItem grow={true}>
+								<EuiTitle size="xs">
+									<h2 id={pushedFlyoutTitleId}>{itemData.content.headline}</h2>
+								</EuiTitle>
+							</EuiFlexItem>
+						</EuiFlexGroup>
 						<EuiSpacer size="xs" />
 						<WireDetail wire={itemData} />
-					</EuiFlyoutBody>
-					<EuiFlyoutFooter>
+					</EuiSplitPanel.Inner>
+					<EuiSplitPanel.Inner>
 						<EuiButton onClick={() => handleDeselectItem()}>Close</EuiButton>
-					</EuiFlyoutFooter>
-				</EuiFlyout>
+					</EuiSplitPanel.Inner>
+				</>
 			)}
-		</EuiPageTemplate.Section>
+		</EuiSplitPanel.Outer>
+	);
+};
+
+const CopyButton = ({ textToCopy }: { textToCopy: string }) => {
+	const [showSuccessIcon, setShowSuccessIcon] = useState(false);
+
+	return (
+		<EuiToolTip
+			position="top"
+			content={
+				<p>
+					Works on any kind of element &mdash; buttons, inputs, you name it!
+				</p>
+			}
+		>
+			<EuiCopy textToCopy={textToCopy}>
+				{(copy) => (
+					<EuiButtonIcon
+						iconType={showSuccessIcon ? 'check' : 'link'}
+						size="s"
+						onClick={() => {
+							copy();
+							setShowSuccessIcon(true);
+							setInterval(() => setShowSuccessIcon(false), 2000);
+						}}
+					>
+						Copy
+					</EuiButtonIcon>
+				)}
+			</EuiCopy>
+		</EuiToolTip>
 	);
 };
