601b5d02e13becc00ae049e4066a8f0a8f3e2f3d
Pete F <pete.faulconbridge@theguardian.com>
2024-10-10 17:21:30 +0100

    Add side nav, hide feed for mobile item view

---
+++ newswires/client/src/SideNav.tsx
@@ -0,0 +1,105 @@
+import {
+	EuiCollapsibleNav,
+	EuiCollapsibleNavGroup,
+	EuiHeaderSectionItemButton,
+	EuiIcon,
+	EuiListGroup,
+} from '@elastic/eui';
+import { useMemo, useState } from 'react';
+import type { Query } from './sharedTypes';
+import { useSearch } from './useSearch';
+
+interface MenuItem {
+	label: string;
+	query: Query;
+}
+
+export const SideNav = () => {
+	const [navIsOpen, setNavIsOpen] = useState<boolean>(false);
+
+	const { state } = useSearch();
+
+	const searchHistory = state.successfulQueryHistory;
+
+	const searchHistoryItems: MenuItem[] = useMemo(
+		() =>
+			searchHistory.map(({ query, resultsCount }) => ({
+				label: `${query.q} (${resultsCount})`,
+				query,
+			})),
+		[searchHistory],
+	);
+
+	const agencies: MenuItem[] = [
+		{ label: 'All', query: { q: '' } },
+		{ label: 'Reuters', query: { q: 'sourceFeed:Reuters' } },
+		{ label: 'AP', query: { q: 'sourceFeed:AP' } },
+		{ label: 'AFP', query: { q: 'sourceFeed:AFP' } },
+	];
+
+	const savedSearches: MenuItem[] = [
+		{ label: 'My saved search', query: { q: 'sourceFeed:Reuters' } },
+		{ label: 'Another saved search', query: { q: 'sourceFeed:AP' } },
+	];
+
+	return (
+		<>
+			<EuiCollapsibleNav
+				isOpen={navIsOpen}
+				isDocked={true}
+				size={240}
+				button={
+					<EuiHeaderSectionItemButton
+						aria-label="Toggle main navigation"
+						onClick={() => setNavIsOpen((isOpen) => !isOpen)}
+					>
+						<EuiIcon type={'menu'} size="m" aria-hidden="true" />
+					</EuiHeaderSectionItemButton>
+				}
+				onClose={() => setNavIsOpen(false)}
+			>
+				<div>
+					<SearchGroup title="Agencies" items={agencies} />
+					<SearchGroup title="Saved searches" items={savedSearches} />
+					<SearchGroup
+						title="Search history"
+						items={searchHistoryItems}
+						isEmptyMessage="No search history available yet"
+					/>
+				</div>
+			</EuiCollapsibleNav>
+		</>
+	);
+};
+
+const SearchGroup = ({
+	title,
+	items,
+	isEmptyMessage,
+}: {
+	title: string;
+	items: MenuItem[];
+	isEmptyMessage?: string;
+}) => {
+	const { handleEnterQuery } = useSearch();
+
+	const listItems =
+		items.length > 0
+			? items.map(({ label, query }) => ({
+					label: label,
+					onClick: () => handleEnterQuery(query),
+				}))
+			: [{ label: isEmptyMessage ?? 'No items available' }];
+
+	return (
+		<EuiCollapsibleNavGroup title={title}>
+			<EuiListGroup
+				listItems={listItems}
+				maxWidth="none"
+				color="subdued"
+				gutterSize="none"
+				size="s"
+			/>
+		</EuiCollapsibleNavGroup>
+	);
+};
