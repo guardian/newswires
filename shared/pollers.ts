1e3c1715b90ff2a7ac641a2d2146c7c9679cc6fd
Tom Richards <tom.richards@guardian.co.uk>
2024-10-28 15:54:13 +0000

    add `poller-lambdas` abstraction (with a number of TODOs remaining)

---
+++ shared/pollers.ts
@@ -0,0 +1,20 @@
+export const POLLER_LAMBDA_ENV_VAR_KEYS = {
+	INGESTION_LAMBDA_QUEUE_URL: 'INGESTION_LAMBDA_QUEUE_URL',
+	OWN_QUEUE_URL: 'OWN_QUEUE_URL',
+} as const;
+
+export interface PollerLambdaProps {
+	overrideLambdaMemoryMB?: number;
+	overrideLambdaTimeoutSeconds?: number;
+}
+
+export type PollerConfig = PollerLambdaProps & {
+	idealFrequencyInSeconds?: number; //TODO some how guarantee that if this is not provided then the function must return something for the next invocation
+};
+
+export type PollerId = keyof typeof POLLERS_CONFIG;
+
+export const POLLERS_CONFIG = {
+	EXAMPLE_long_polling: {},
+	EXAMPLE_fixed_frequency: { idealFrequencyInSeconds: 30 },
+} as const satisfies Record<string, PollerConfig>; // used to generate lambda etc. per agency, with config mapped
