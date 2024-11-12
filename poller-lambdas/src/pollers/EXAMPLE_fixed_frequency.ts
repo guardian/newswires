b1de33aee1a94dd7e5cdbca980e5b640264fa761
Pete F <pete.faulconbridge@theguardian.com>
2024-11-12 16:14:55 +0000

    Add lint script and fix lint errors

--- poller-lambdas/src/pollers/EXAMPLE_fixed_frequency.ts
+++ poller-lambdas/src/pollers/EXAMPLE_fixed_frequency.ts
@@ -1,10 +1,11 @@
-import { FixedFrequencyPollFunction, SecretValue } from '../types';
+import type { FixedFrequencyPollFunction, SecretValue } from '../types';
 
-export default (async (secret: SecretValue) => {
+export const EXAMPLE_fixed_frequency = (async (secret: SecretValue) => {
 	console.log({
 		secretLength: secret.length,
 		pollingAt: new Date().toISOString(),
 	});
+	await new Promise((resolve) => setTimeout(resolve, 10));
 	return {
 		payloadForIngestionLambda: {},
 		idealFrequencyInSeconds: 30,
--- poller-lambdas/src/pollers/EXAMPLE_long_polling.ts
+++ poller-lambdas/src/pollers/EXAMPLE_long_polling.ts
@@ -1,6 +1,9 @@
-import { LongPollFunction, PollerInput, SecretValue } from '../types';
+import type { LongPollFunction, PollerInput, SecretValue } from '../types';
 
-export default (async (secret: SecretValue, input: PollerInput) => {
+export const EXAMPLE_long_polling = (async (
+	secret: SecretValue,
+	input: PollerInput,
+) => {
 	const previousCounterValue = parseInt(input);
 	await new Promise((resolve) => setTimeout(resolve, 30000)); // simulate long poll taking 30s
 	const newCounterValue = previousCounterValue + 1;
