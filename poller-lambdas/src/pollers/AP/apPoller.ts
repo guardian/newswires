2c785bf11739a9ea16cf5385ce597ba881c11bef
Pete F <pete.faulconbridge@theguardian.com>
2024-12-12 15:47:28 +0000

    Fix import and URL trailing slash

--- poller-lambdas/src/pollers/AP/apPoller.ts
+++ poller-lambdas/src/pollers/AP/apPoller.ts
@@ -18,9 +18,10 @@ type FeedItemWithContent = {
 	contentFromNitf: ContentFromNitf;
 };
 
+// https://api.ap.org/media/v-preview/content/feed?page_size=10&in_my_plan=true
 export const apPoller = (async (secret: SecretValue, input: PollerInput) => {
 	// todo: remove '-preview' from baseUrl when we go live
-	const baseUrl = 'https://api.ap.org/media/v-preview/';
+	const baseUrl = 'https://api.ap.org/media/v-preview';
 	const defaultFeedUrl = `${baseUrl}/content/feed?page_size=10&in_my_plan=true`;
 	const apiKey = secret;
 
