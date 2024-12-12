3558796f13fca325ed26910f95ed69699c28a97a
Pete F <pete.faulconbridge@theguardian.com>
2024-12-12 15:36:42 +0000

    Wire up AP poller

--- poller-lambdas/src/index.ts
+++ poller-lambdas/src/index.ts
@@ -1,12 +1,11 @@
 import { GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
-import {
-	SendMessageCommand,
-	SendMessageCommandInput,
-} from '@aws-sdk/client-sqs';
+import type { SendMessageCommandInput } from '@aws-sdk/client-sqs';
+import { SendMessageCommand } from '@aws-sdk/client-sqs';
 import type { PollerId } from '../../shared/pollers';
 import { POLLER_LAMBDA_ENV_VAR_KEYS } from '../../shared/pollers';
 import { queueNextInvocation, secretsManager, sqs } from './aws';
 import { getEnvironmentVariableOrCrash } from './config';
+import { apPoller } from './pollers/AP/apPoller';
 import { EXAMPLE_fixed_frequency } from './pollers/EXAMPLE_fixed_frequency';
 import { EXAMPLE_long_polling } from './pollers/EXAMPLE_long_polling';
 import type { HandlerInputSqsPayload, PollFunction } from './types';
@@ -105,6 +104,7 @@ const pollerWrapper =
 export = {
 	EXAMPLE_long_polling: pollerWrapper(EXAMPLE_long_polling),
 	EXAMPLE_fixed_frequency: pollerWrapper(EXAMPLE_fixed_frequency),
+	apPoller: pollerWrapper(apPoller),
 } satisfies Record<
 	PollerId,
 	(sqsEvent: HandlerInputSqsPayload) => Promise<void>
