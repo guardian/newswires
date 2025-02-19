b9a87b8a1eca0a96b4dd446562ef49ce643657cd
Pete F <pete.faulconbridge@theguardian.com>
2025-02-19 09:35:29 +0000

    Update migration to be 'if not exists'

--- db/migrations/V13__category_codes_column.sql
+++ db/migrations/V13__category_codes_column.sql
@@ -1,4 +1,4 @@
 ALTER TABLE fingerpost_wire_entry
-    ADD category_codes text[];
+    ADD COLUMN IF NOT EXISTS category_codes text[];
 
-CREATE INDEX category_codes_index ON fingerpost_wire_entry(category_codes);
+CREATE INDEX IF NOT EXISTS category_codes_index ON fingerpost_wire_entry(category_codes);
