602dbc3943a36b28f7ffb010a41cec1f268168bc
Pete F <pete.faulconbridge@theguardian.com>
2025-04-03 15:27:44 +0100

    Pass stage as client config

--- newswires/client/src/serverSideConfig.ts/serverSideConfig.ts
+++ newswires/client/src/serverSideConfig.ts/serverSideConfig.ts
@@ -2,3 +2,5 @@ export const SUPPLIERS_TO_EXCLUDE = window.configuration.switches
 	.ShowGuSuppliers
 	? []
 	: ['GUAP', 'GUREUTERS'];
+
+export const stage = window.configuration.stage;
--- newswires/client/src/serverSideConfig.ts/windowConfigType.ts
+++ newswires/client/src/serverSideConfig.ts/windowConfigType.ts
@@ -9,6 +9,7 @@ declare global {
 	interface Window {
 		configuration: {
 			switches: FeatureSwitches;
+			stage: string;
 		};
 	}
 }
