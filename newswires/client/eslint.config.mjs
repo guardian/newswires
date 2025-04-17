c7fb49730bda7aceca63e513558e9a8414ba16a5
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-04-17 11:03:03 +0100

    eslint 9 upgrade

---
+++ newswires/client/eslint.config.mjs
@@ -0,0 +1,22 @@
+import guardian from '@guardian/eslint-config';
+import storybook from 'eslint-plugin-storybook';
+import newswiresConfig from '../../eslint.config.mjs';
+
+export default [
+	...newswiresConfig,
+	...guardian.configs.react,
+	...storybook.configs['flat/recommended'],
+	{
+		rules: {
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+			'no-restricted-syntax': [
+				'error',
+				{
+					message:
+						"Don't use `fetch` directly, please use the `pandaFetch` abstraction, to get automatic session refreshes",
+					selector: "CallExpression[callee.name='fetch']",
+				},
+			],
+		},
+	},
+];
