a56c036a608df043923e3c0407611b19814e1ea4
Pete F <pete.faulconbridge@theguardian.com>
2025-04-18 20:34:50 +0100

    Don't show date info in search summary if it's the same as the default date range

--- newswires/client/src/dateConstants.ts
+++ newswires/client/src/dateConstants.ts
@@ -7,7 +7,4 @@ export const TWO_WEEKS_AGO = moment().subtract(2, 'weeks');
 export const START_OF_TODAY = moment().startOf('day');
 export const END_OF_TODAY = moment().endOf('day');
 
-export const DEFAULT_DATE_RANGE = {
-	start: TODAY,
-	end: TODAY,
-};
+export const DEFAULT_DATE_RANGE = { start: TODAY, end: TODAY };
