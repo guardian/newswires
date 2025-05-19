107ea2d218241e6b9a9d989cc367f6c905303224
Pete F <pete.faulconbridge@theguardian.com>
2025-05-19 16:38:10 +0100

    Fix config for test environments

--- newswires/client/src/app-configuration.ts
+++ newswires/client/src/app-configuration.ts
@@ -1,6 +1,24 @@
-export const SUPPLIERS_TO_EXCLUDE = window.configuration.switches
-	.ShowGuSuppliers
+const isNode = typeof process !== 'undefined';
+const isJest = isNode && process.env.JEST_WORKER_ID !== undefined;
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- if the code isn't
+if (isJest === undefined && !window.configuration) {
+	throw new Error('window object is defined, but window.configuration is not');
+}
+
+const configLookup = isJest
+	? {
+			switches: {
+				ShowGuSuppliers: false,
+			},
+			stage: 'test',
+		}
+	: window.configuration;
+
+const showGuSuppliers = configLookup.switches.ShowGuSuppliers;
+
+export const SUPPLIERS_TO_EXCLUDE = showGuSuppliers
 	? []
 	: ['GUAP', 'GUREUTERS'];
 
-export const stage = window.configuration.stage;
+export const stage = configLookup.stage;
--- newswires/client/src/windowConfigType.ts
+++ newswires/client/src/windowConfigType.ts
@@ -2,15 +2,17 @@ interface FeatureSwitches {
 	ShowGuSuppliers: boolean;
 }
 
+export interface AppConfiguration {
+	switches: FeatureSwitches;
+	stage: string;
+}
+
 declare global {
 	/* ~ Here, declare things that go in the global namespace, or augment
 	 *~ existing declarations in the global namespace
 	 */
 	interface Window {
-		configuration: {
-			switches: FeatureSwitches;
-			stage: string;
-		};
+		configuration: AppConfiguration;
 	}
 }
 /* ~ this line is required as per TypeScript's global-modifying-module.d.ts instructions */
