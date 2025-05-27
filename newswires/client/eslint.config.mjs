54e955ee21c051cb83d1ba542448a93af3509004
Pete F <pete.faulconbridge@theguardian.com>
2025-05-27 14:48:37 +0100

    Re-add prettier (as part of eslint config)

--- newswires/client/eslint.config.mjs
+++ newswires/client/eslint.config.mjs
@@ -1,11 +1,14 @@
 import guardian from '@guardian/eslint-config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import storybook from 'eslint-plugin-storybook';
 import newswiresConfig from '../../eslint.config.mjs';
+
 
 export default [
 	...newswiresConfig,
 	...guardian.configs.react,
 	...storybook.configs['flat/recommended'],
+	eslintPluginPrettierRecommended,
 	{
 		rules: {
       "@typescript-eslint/switch-exhaustiveness-check": [
