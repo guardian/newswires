e056fed5827ab8ba8121de1db5895ab010e4dcf7
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-07-11 12:05:55 +0100

    send the whole api data, not just the content

--- newswires/app/db/FingerpostWireEntry.scala
+++ newswires/app/db/FingerpostWireEntry.scala
@@ -170,6 +170,16 @@ object FingerpostWireEntry
       .apply()
   }
 
+  case class QueryResponse(
+      results: List[FingerpostWireEntry],
+      totalCount: Long
+//      keywordCounts: Map[String, Int]
+  )
+
+  private object QueryResponse {
+    implicit val writes: OWrites[QueryResponse] = Json.writes[QueryResponse]
+  }
+
   def getRaw(id: Int): Option[String] = DB readOnly { implicit session =>
     sql"""| SELECT $selectAllStatement
           | FROM ${FingerpostWireEntry as syn}
