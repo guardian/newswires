f0ec408a322deeffb87a60536ce12dc4508a55bb
lindseydew <lindsey.dew@guardian.co.uk>
2025-07-31 15:25:01 +0100

    Maintain background colours in the table

--- newswires/client/src/htmlFormatHelpers.test.ts
+++ newswires/client/src/htmlFormatHelpers.test.ts
@@ -5,4 +5,8 @@ describe('htmlFormatBody', () => {
 		const tablehtml = `<table><tr><td colspan="2">Test</td></tr></table>`;
 		expect(htmlFormatBody(tablehtml)).toBe(tablehtml);
 	});
+	it('should maintain style attributes in a table definition', () => {
+		const tablehtml = `<table><tr><td style="background-color:#EFE704">Test</td></tr></table>`;
+		expect(htmlFormatBody(tablehtml)).toBe(tablehtml);
+	});
 });
--- newswires/client/src/htmlFormatHelpers.ts
+++ newswires/client/src/htmlFormatHelpers.ts
@@ -4,7 +4,7 @@ export const htmlFormatBody = (bodyText: string) => {
 	return sanitizeHtml(bodyText, {
 		allowedAttributes: {
 			...sanitizeHtml.defaults.allowedAttributes,
-			td: ['colspan'],
+			td: ['colspan', 'style'],
 		},
 	});
 };
