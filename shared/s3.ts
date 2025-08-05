ad0d53f58a3122e0052a1857ec973922a0149033
Pete F <pete.faulconbridge@theguardian.com>
2025-08-05 14:44:15 +0100

    Fix: don't try to export email bucket name from shared module

--- shared/s3.ts
+++ shared/s3.ts
@@ -95,7 +95,3 @@ export async function putToS3({
 export const FEEDS_BUCKET_NAME: string = isRunningLocally
 	? 'local-feeds-bucket'
 	: getFromEnv('FEEDS_BUCKET_NAME');
-
-export const EMAIL_BUCKET_NAME: string = isRunningLocally
-	? 'local-email-bucket'
-	: getFromEnv('EMAIL_BUCKET_NAME');
