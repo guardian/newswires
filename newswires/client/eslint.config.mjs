9ac3398d5685f3aff9a55e73e9ae132e6e3ad4eb
Pete F <pete.faulconbridge@theguardian.com>
2025-08-05 09:29:14 +0100

    Remove storybook
    I'm a fan of storybook, but realistically I've spent quite a bit
    more time keeping it up to date than I have spent really using it, so
    far, and I think that holds for others who've worked on the project,
    too.

--- newswires/client/eslint.config.mjs
+++ newswires/client/eslint.config.mjs
@@ -1,11 +1,9 @@
 import guardian from '@guardian/eslint-config';
-import storybook from 'eslint-plugin-storybook';
 import newswiresConfig from '../../eslint.config.mjs';
 
 export default [
 	...newswiresConfig,
 	...guardian.configs.react,
-	...storybook.configs['flat/recommended'],
 	{
 		rules: {
 			'@typescript-eslint/switch-exhaustiveness-check': [
