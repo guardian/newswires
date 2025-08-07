f180b78283ec283840299cdccb6b2ea63e0e0894
Pete F <pete.faulconbridge@theguardian.com>
2025-08-07 15:18:56 +0100

    WIP: Wire cache provider in

--- newswires/app/db/FingerpostWireEntry.scala
+++ newswires/app/db/FingerpostWireEntry.scala
@@ -33,7 +33,8 @@ case class FingerpostWireEntry(
 
 object FingerpostWireEntry
     extends SQLSyntaxSupport[FingerpostWireEntry]
-    with Logging {
+    with Logging
+    with DatabaseProvider {
 
   implicit val jsonEncoder: Encoder[FingerpostWireEntry] =
     deriveEncoder[FingerpostWireEntry].mapJson(_.dropNullValues)
@@ -393,7 +394,7 @@ object FingerpostWireEntry
            | """.stripMargin
   }
 
-  def query(
+  override def query(
       queryParams: QueryParams
   ): QueryResponse = DB readOnly { implicit session =>
     val whereClause = buildWhereClause(
