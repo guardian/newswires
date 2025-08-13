4f1fd25bbc359b35f6a2cedb260bd19abd4eaa64
Pete F <pete.faulconbridge@theguardian.com>
2025-08-13 17:42:33 +0100

    Refactor tests to clarify expected behaviour in more detail

--- newswires/test/controllers/QueryControllerSpec.scala
+++ newswires/test/controllers/QueryControllerSpec.scala
@@ -7,23 +7,19 @@ class QueryControllerSpec extends AnyFlatSpec with Matchers {
 
   behavior of "QueryController.effectiveSuppliersToExclude"
 
-  it should "exclude UNAUTHED_EMAIL_FEED by default" in {
+  it should "exclude UNAUTHED_EMAIL_FEED and the Gu supplier feeds by default if 'showGuSuppliers' is false" in {
     val suppliersExcl = QueryController.effectiveSuppliersToExclude(
       suppliersExclFromRequest = Nil,
       suppliersToInclude = Nil,
       showGuSuppliers = false
     )
-    suppliersExcl should contain("UNAUTHED_EMAIL_FEED")
-  }
 
-  it should "exclude GuReuters and GuAP if showGuSuppliers is false" in {
-    val suppliersExcl = QueryController.effectiveSuppliersToExclude(
-      suppliersExclFromRequest = Nil,
-      suppliersToInclude = Nil,
-      showGuSuppliers = false
+    suppliersExcl should contain allOf (
+      "UNAUTHED_EMAIL_FEED",
+      "GuReuters",
+      "GuAP"
     )
-    suppliersExcl should contain("GuReuters")
-    suppliersExcl should contain("GuAP")
+    suppliersExcl.length shouldBe 3
   }
 
   it should "not exclude GuReuters and GuAP if showGuSuppliers is true" in {
@@ -40,9 +36,10 @@ class QueryControllerSpec extends AnyFlatSpec with Matchers {
     val suppliersExcl = QueryController.effectiveSuppliersToExclude(
       suppliersExclFromRequest = List("Reuters"),
       suppliersToInclude = Nil,
-      showGuSuppliers = false
+      showGuSuppliers = true
     )
-    suppliersExcl should contain("Reuters")
+    suppliersExcl should contain allOf ("Reuters", "UNAUTHED_EMAIL_FEED")
+    suppliersExcl.length shouldBe 2
   }
 
   it should "allow default exclusions to be overridden by explicit inclusion" in {
@@ -56,7 +53,13 @@ class QueryControllerSpec extends AnyFlatSpec with Matchers {
     suppliersExcl should contain("GuAP")
   }
 
-  it should "not currently try to reconcile explicit inclusions and exclusions" in {
+  it should "not allow *explicit* exclusions to be overridden by explicit inclusion" in {
+
+    /** Including and excluding the same supplier is pointless, but if both
+      * settings have been set explicitly by the caller, it's not really the
+      * business of this function to try and reconcile them. It will just return
+      * the suppliers to exclude as specified.
+      */
     val suppliersExcl = QueryController.effectiveSuppliersToExclude(
       suppliersExclFromRequest = List("ABC"),
       suppliersToInclude = List("ABC"),
