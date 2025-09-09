4475058a009fa1512c3563c0a14d4b9eaf8c33fd
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-09 17:23:08 +0100

    Run linter

--- ingestion-lambda/src/db.test.ts
+++ ingestion-lambda/src/db.test.ts
@@ -1,8 +1,8 @@
-import { putItemToDb } from "./db";
+import postgres from 'postgres';
+import { DATABASE_TABLE_NAME } from '../../shared/constants';
 import * as loggingModule from '../../shared/lambda-logging';
-import postgres from "postgres";
-import {  DATABASE_PORT } from "../../shared/rds";
-import { DATABASE_TABLE_NAME } from "../../shared/constants";
+import { DATABASE_PORT } from '../../shared/rds';
+import { putItemToDb } from './db';
 
 // and even the lambda-logging module
 jest.mock('../../shared/lambda-logging', () => {
@@ -19,41 +19,42 @@ jest.mock('../../shared/lambda-logging', () => {
 });
 const mockCreateLogger = loggingModule.createLogger;
 
-
-describe('putItemToDb', () => { 
-
-    const sql = postgres({
-            port: DATABASE_PORT,
-            hostname: 'localhost',
-            username: 'postgres',
-            database: 'newswires',
-            password: 'testpassword',
-    });
-
-    beforeEach(async () => {
-		jest.clearAllMocks();
-		jest.resetAllMocks();        
-        await sql`TRUNCATE TABLE ${sql(DATABASE_TABLE_NAME)};`
-
+describe('putItemToDb', () => {
+	const sql = postgres({
+		port: DATABASE_PORT,
+		hostname: 'localhost',
+		username: 'postgres',
+		database: 'newswires',
+		password: 'testpassword',
 	});
 
-    afterAll(async () => {
-        await sql.end();
-    });
+	beforeEach(async () => {
+		jest.clearAllMocks();
+		jest.resetAllMocks();
+		await sql`TRUNCATE TABLE ${sql(DATABASE_TABLE_NAME)};`;
+	});
 
-    it('should be able to insert an item into the database', async () => {
-        await putItemToDb({
-            processedObject: { content: {
-                slug: 'test-slug',
-                keywords: [],
-                imageIds: []
-            }, supplier: 'test-supplier', categoryCodes: ['code1', 'code2'] },
-            externalId: 'test-external-id',
-            s3Key: 'test-s3-key',
-            sql: sql,
-            logger: mockCreateLogger({}),
-        });
-        const results = await sql`SELECT * FROM ${sql(DATABASE_TABLE_NAME)};`
-        expect(results.length).toBe(1);
-    });
-})
+	afterAll(async () => {
+		await sql.end();
+	});
+
+	it('should be able to insert an item into the database', async () => {
+		await putItemToDb({
+			processedObject: {
+				content: {
+					slug: 'test-slug',
+					keywords: [],
+					imageIds: [],
+				},
+				supplier: 'test-supplier',
+				categoryCodes: ['code1', 'code2'],
+			},
+			externalId: 'test-external-id',
+			s3Key: 'test-s3-key',
+			sql: sql,
+			logger: mockCreateLogger({}),
+		});
+		const results = await sql`SELECT * FROM ${sql(DATABASE_TABLE_NAME)};`;
+		expect(results.length).toBe(1);
+	});
+});
