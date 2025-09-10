0dbd95441059b594919a2871e26176fc5dded438
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-10 14:38:04 +0100

    Add a check to see if the db is running

--- scripts/setup-test-env.sh
+++ scripts/setup-test-env.sh
@@ -1,4 +1,11 @@
-echo "🔧 Starting test database..."
-docker compose -f docker-compose.test.yml up -d --wait
-echo "🚀 Applying migrations..."
-yes | ./db/flyway.sc migrate test
+#!/bin/bash
+set -e
+echo "🔍 Checking to see if the database is up"
+if docker compose -f docker-compose.test.yml ps | grep -q "newswires-test-db.*Up"; then
+  echo "✅ Database is already running"
+else
+    echo "🔧 Starting test database..."
+    docker compose -f docker-compose.test.yml up -d --wait
+    echo "🚀 Applying migrations..."
+    yes | ./db/flyway.sc migrate test
+fi
