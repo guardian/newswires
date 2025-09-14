dac864e200fe2622e32366d6becadb1f8d6e0f0b
Mateusz <mateusz.karpow@guardian.co.uk>
2025-09-14 16:25:18 +0100

    Remove further soccer (results) from No Soccer

--- newswires/app/conf/SearchPresets.scala
+++ newswires/app/conf/SearchPresets.scala
@@ -264,8 +264,9 @@ object SearchPresets {
       categoryCodes = CategoryCodes.Sport.REUTERS.filterNot(CategoryCodes.Soccer.REUTERS.contains),
       categoryCodesExcl = CategoryCodes.Soccer.REUTERS
     ),
-    SearchPreset(
+    SearchPreset.fromSearchTerm(
       PA,
+      searchTerm = SearchTerm.Simple("-SOCCER", Slug),
       categoryCodes = CategoryCodes.Sport.PA.filterNot(CategoryCodes.Soccer.PA.contains),
       categoryCodesExcl = CategoryCodes.Soccer.PA
     ),
@@ -278,7 +279,10 @@ object SearchPresets {
       categoryCodes = CategoryCodes.Sport.AAP.filterNot(CategoryCodes.Soccer.AAP.contains),
       categoryCodesExcl = CategoryCodes.Soccer.AAP
     ),
-    SearchPreset(AP, CategoryCodes.Sport.AP, keywordExcl = List("Soccer"))
+    SearchPreset.fromSearchTerm(
+      AP,
+      searchTerm = SearchTerm.Simple("-BC -SOC", Slug),
+      CategoryCodes.Sport.AP, keywordExcl = List("Soccer"))
   )
 
   private val Cricket = List(
