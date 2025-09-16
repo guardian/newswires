2ba7ef495dd082f614563af65fd56c6ceb2c7422
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-16 15:11:50 +0100

    Simplify the ap world preset

--- newswires/app/conf/SearchPresets.scala
+++ newswires/app/conf/SearchPresets.scala
@@ -118,12 +118,6 @@ object SearchPresets {
   private val ApWorld = List(
     SearchPreset(
       AP,
-      keywords = List("World news", "U.S. news"),
-      categoryCodes = CategoryCodes.World.AP,
-      categoryCodesExcl = CategoryCodes.Sport.AP ::: CategoryCodes.Business.AP ::: CategoryCodes.Other.AP
-    ),
-    SearchPreset(
-      AP,
       keywords = List("General news"),
       categoryCodes = CategoryCodes.World.AP
     )
