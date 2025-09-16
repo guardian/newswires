7cc326f3667f0c450576538a7be7692ba53dec3d
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-16 14:57:11 +0100

    Return the last migration in endpoint

--- db/db-migrations-checker/src/router.ts
+++ db/db-migrations-checker/src/router.ts
@@ -1,11 +1,14 @@
 import type { Request } from 'express';
 import { Router } from 'express';
 import type { Response } from 'express';
-
+import { initialiseDbConnection } from '../../../shared/rds';
 const router: Router = Router();
 
-router.get('/migrations', (req: Request, res: Response) => {
-	return res.send({ response: 'ok' });
+router.get('/migrations', async (_: Request, res: Response) => {
+    const { sql, closeDbConnection } = await initialiseDbConnection();
+    const migrations = await sql`SELECT script FROM flyway_schema_history order by installed_on desc limit 1`;
+    await closeDbConnection();
+	return res.send({ migrations });
 });
 
 export default router;
