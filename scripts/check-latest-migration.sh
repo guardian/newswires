211175752fbfe54fcb681526a4957396d4b69f2e
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-23 14:04:54 +0100

    Update variable names

--- scripts/check-latest-migration.sh
+++ scripts/check-latest-migration.sh
@@ -1,11 +1,11 @@
 VERSION=$(aws ssm get-parameter --name "/CODE/editorial-feeds/newswires/database/latest-migration-version" --query "Parameter.Value" --output text --region eu-west-1)
 echo "Latest migration version in SSM Parameter Store: $VERSION"
 
-files=$(ls ../db/migrations)
-for f in $files; do
-   version_number=$(echo $f | sed -E 's/^V([0-9]+)__.*$/\1/')
-   if [ "$version_number" -gt "$VERSION" ]; then
-     echo "❌ There are new migrations that have not been applied. Latest migration file is $f"
+FILES=$(ls ../db/migrations)
+for file in $FILES; do
+   VERSION_NUMBER=$(echo $file | sed -E 's/^V([0-9]+)__.*$/\1/')
+   if [ "$VERSION_NUMBER" -gt "$VERSION" ]; then
+     echo "❌ There are new migrations that have not been applied. Latest migration file is $file"
      exit 1
    fi
 done
