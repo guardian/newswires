58ee3921f9d896914aa8154844cc6db48561ba21
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-30 16:30:38 +0100

    Persist the presets

--- reingestion-lambda/src/handler.ts
+++ reingestion-lambda/src/handler.ts
@@ -15,18 +15,27 @@ async function getRecords(sql: Sql, limit: number) {
 const computePresetCategories = (categoryCodes: string[]) => {
 	return categoryCodes.filter((code) => code.startsWith('qwertyuiop'));
 };
+const toPostgressArray = (classifications: string[]) => {
+	if (classifications.length === 0) return 'ARRAY[]::text[]';
+	return `ARRAY[${classifications.map((c) => `'${c}'`).join(',')}]`;
+};
 async function updateRecords(sql: Sql, records: DBRecord[]) {
-	await sql`update fingerpost_wire_entry as fwe 
-        set last_updated_at = now(), 
+	const values = records
+		.map(
+			(record) =>
+				`('${record.external_id}', ${toPostgressArray(computePresetCategories(record.category_codes))})`,
+		)
+		.join(',');
+	await sql.unsafe(`
+        UPDATE fingerpost_wire_entry AS fwe
+        SET 
+            last_updated_at = NOW(), 
             preset_categories = data.preset_categories
-        from (values ${sql(
-					records.map((r) => {
-						return {
-							external_id: r.external_id,
-							preset_categories: computePresetCategories(r.category_codes),
-						};
-					}),
-				)}) as data(external_id, preset_categories) where fwe.external_id = data.external_id;`;
+        FROM (
+            VALUES ${values}
+        ) AS data(external_id, preset_categories)
+        WHERE fwe.external_id = data.external_id;
+    `);
 }
 
 export const main = async ({
