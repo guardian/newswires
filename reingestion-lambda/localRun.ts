38d5d3d8ec8e17b624324f3ba1933572f3d16c41
lindseydew <lindsey.dew@guardian.co.uk>
2025-09-30 17:34:47 +0100

    Add in batches

--- reingestion-lambda/localRun.ts
+++ reingestion-lambda/localRun.ts
@@ -2,6 +2,7 @@ import { main } from "./src/handler";
 async function run() {
     main({
         limit: 25,
+        batchSize: 10,
         timeDelay: 1000
     })
 }
--- reingestion-lambda/src/handler.ts
+++ reingestion-lambda/src/handler.ts
@@ -7,9 +7,9 @@ type DBRecord = {
 	category_codes: string[];
 };
 
-async function getRecords(sql: Sql, limit: number) {
+async function getRecords(sql: Sql, limit: number, offset: number) {
 	const results =
-		await sql`SELECT external_id, category_codes FROM fingerpost_wire_entry ORDER BY id LIMIT ${limit};`;
+		await sql`SELECT external_id, category_codes FROM fingerpost_wire_entry ORDER BY id LIMIT ${limit} offset ${offset};`;
 	return results.map((r) => r as DBRecord);
 }
 
@@ -29,8 +29,7 @@ async function updateRecords(sql: Sql, records: DBRecord[]) {
 				`('${record.external_id}', ${toPostgressArray(computePresetCategories(record.category_codes))})`,
 		)
 		.join(',');
-	await sql.unsafe(`
-        UPDATE fingerpost_wire_entry AS fwe
+	await sql.unsafe(`UPDATE fingerpost_wire_entry AS fwe
         SET 
             last_updated_at = NOW(), 
             preset_categories = data.preset_categories
@@ -41,18 +40,38 @@ async function updateRecords(sql: Sql, records: DBRecord[]) {
     `);
 }
 
+function computeOffsets(max: number, batchSize: number): number[] {
+	const batches = Math.ceil(max / batchSize);
+	return Array.from({ length: batches }, (_, i) => i * batchSize);
+}
+
 export const main = async ({
 	limit,
+	batchSize,
 	timeDelay,
 }: {
 	limit: number;
+	batchSize: number;
 	timeDelay: number;
 }) => {
 	const { sql, closeDbConnection } = await initialiseDbConnection();
-	const records = await getRecords(sql, limit);
-	console.log(`Fetched ${records.length} records from the database.`);
-	await updateRecords(sql, records);
-	console.log(`Updated ${records.length} records in the database.`);
-	await new Promise((resolve) => setTimeout(resolve, timeDelay));
+	const offsets = computeOffsets(limit, batchSize);
+	console.info(
+		`Starting to update up to ${limit} records in batches of ${batchSize}`,
+	);
+	console.log(offsets);
+	for (const [index, offset] of offsets.entries()) {
+		const records = await getRecords(sql, batchSize, offset);
+		console.info(`Processing batch ${index + 1} of ${offsets.length}`);
+		if (records.length === 0) {
+			console.info('No more records to process, exiting');
+			return;
+		}
+		console.info(`Updating ${records.length} records`);
+		await updateRecords(sql, records);
+		console.info(`Finished processing batch ${index + 1} of ${offsets.length}`);
+		await new Promise((resolve) => setTimeout(resolve, timeDelay));
+	}
 	await closeDbConnection();
+	console.info(`Finished updating ${limit} records`);
 };
