6f5d0c3383eb519e3ae23fe3d271358f441605ae
lindseydew <lindsey.dew@guardian.co.uk>
2025-10-01 12:59:17 +0100

    Fill in the entries where last_updated is not defined

--- reingestion-lambda/localRun.ts
+++ reingestion-lambda/localRun.ts
@@ -3,7 +3,7 @@ async function run() {
     main({
         limit: 25,
         batchSize: 10,
-        timeDelay: 1000
+        timeDelay: 1000,
     })
 }
 run()
--- reingestion-lambda/src/handler.ts
+++ reingestion-lambda/src/handler.ts
@@ -9,7 +9,7 @@ type DBRecord = {
 
 async function getRecords(sql: Sql, limit: number, offset: number) {
 	const results =
-		await sql`SELECT external_id, category_codes FROM fingerpost_wire_entry ORDER BY id LIMIT ${limit} offset ${offset};`;
+		await sql`SELECT external_id, category_codes FROM fingerpost_wire_entry ORDER BY id LIMIT ${limit} offset ${offset} and last_updated_at is null;`;
 	return results.map((r) => r as DBRecord);
 }
 
