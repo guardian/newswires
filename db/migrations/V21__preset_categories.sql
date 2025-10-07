bea094342d379908e6631fe4d828f38fd9891ef9
lindseydew <lindsey.dew@guardian.co.uk>
2025-10-07 17:18:22 +0100

    Remove the extra migrations steps

--- db/migrations/V21__preset_categories.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE fingerpost_wire_entry ADD COLUMN preset_categories text[];
-CREATE INDEX IF NOT EXISTS preset_categories_index ON fingerpost_wire_entry(preset_categories);
--- db/migrations/V23__preset_categories_index.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS preset_categories_index;
-CREATE INDEX IF NOT EXISTS preset_categories_index ON fingerpost_wire_entry USING GIN(preset_categories);
