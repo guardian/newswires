11dedf416c6e4e67e6a6420f5b74539cade4160f
lindseydew <lindsey.dew@guardian.co.uk>
2025-10-09 11:10:00 +0100

    Refactor query

--- reingestion-lambda/src/query.ts
+++ reingestion-lambda/src/query.ts
@@ -31,15 +31,20 @@ const getQuery = async (
 	lastUpdatedSince?: Date,
 	lastUpdatedUntil?: Date,
 ): Promise<DBRecord[]> => {
-	let baseQuery = `SELECT external_id, category_codes FROM ${DATABASE_TABLE_NAME}`;
 	const conditions = whereClause(sql, { lastUpdatedSince, lastUpdatedUntil });
-	if (conditions.length > 0) {
-		baseQuery += ` WHERE ${conditions.join(' AND ')}`;
-	}
-	baseQuery += ` ORDER BY id LIMIT ${limit} OFFSET ${offset};`;
-	const results = await sql`${baseQuery}`;
-	return results.map((r) => {
-		return { external_id: r.external_id, category_codes: r.category_codes };
+	const where = conditions.length
+		? conditions.reduce(
+				(prev, curr, i) => {
+					if (i === 0) return sql`where ${curr}`;
+					return sql`${prev} and ${curr}`;
+				},
+				sql``,
+			)
+		: sql``;
+	const results =
+		await sql`SELECT external_id, category_codes FROM ${sql(DATABASE_TABLE_NAME)} ${where} ORDER BY id LIMIT ${limit} OFFSET ${offset};`;
+	return results.map((r: Row) => {
+		return r as DBRecord;
 	});
 };
 const countQuery = async (
@@ -47,12 +52,19 @@ const countQuery = async (
 	lastUpdatedSince?: Date,
 	lastUpdatedUntil?: Date,
 ) => {
-	let baseQuery = sql`SELECT COUNT(*) FROM ${DATABASE_TABLE_NAME}`;
 	const conditions = whereClause(sql, { lastUpdatedSince, lastUpdatedUntil });
-	if (conditions.length > 0) {
-		baseQuery = sql`${baseQuery} WHERE ${conditions.join(' AND ')}`;
-	}
-	const results = await sql(`${baseQuery}`);
+	const where = conditions.length
+		? conditions.reduce(
+				(prev, curr, i) => {
+					if (i === 0) return sql`where ${curr}`;
+					return sql`${prev} and ${curr}`;
+				},
+				sql``,
+			)
+		: sql``;
+
+	const results =
+		await sql`SELECT COUNT(*) FROM ${sql(DATABASE_TABLE_NAME)} ${where}`;
 	if (results.length > 0 && results[0] !== undefined)
 		return Number(results[0].count);
 	return 0;
@@ -74,10 +86,10 @@ const whereClause = (
 ): Array<PendingQuery<Row[]>> => {
 	const conditions = Object.entries(params)
 		.map(([k, v]) => {
-			if (k !== undefined && k in whereConditions && v !== undefined) {
+			if (k in whereConditions) {
 				const key = k as WhereKey;
 				return whereConditions[key](v);
-			}
+			} else return undefined;
 		})
 		.filter((p) => p !== undefined);
 	return conditions.map((c) => sql`${c}`);
