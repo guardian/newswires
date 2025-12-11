7e8ee80f63301559f1ee47da7039d8bceac4414b
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-12-11 17:33:46 +0000

    add second authentication pass for incoming mails to try to validate via ARC

--- shared/src/findVerificationFailures.ts
+++ shared/src/findVerificationFailures.ts
@@ -1,7 +1,51 @@
-import type { SESReceipt } from 'aws-lambda';
+import type { SESMessage } from 'aws-lambda';
+import { authenticate } from 'mailauth';
+import './typings/mailauth.d.ts';
+import { getFromEnv, isRunningLocally } from './config';
+import { getFromS3 } from './s3';
 
-export function findVerificationFailures(receipt: SESReceipt) {
-	const verdictChecks = [
+type EmailVerificationCheck = {
+	name: string;
+	status: string;
+};
+type EmailVerificationResult = {
+	pass: boolean;
+	failedChecks: EmailVerificationCheck[];
+};
+
+const EMAIL_BUCKET_NAME: string = isRunningLocally
+	? 'local-email-bucket'
+	: getFromEnv('EMAIL_BUCKET_NAME');
+
+const DOTCOPY_EMAIL_ADDRESS = isRunningLocally
+	? 'FIXME'
+	: getFromEnv('DOTCOPY_EMAIL_ADDRESS');
+
+const dotcopyUsernameEnd = DOTCOPY_EMAIL_ADDRESS.indexOf('@');
+const dotcopyUsername = DOTCOPY_EMAIL_ADDRESS.slice(0, dotcopyUsernameEnd);
+
+const validForCopy = (forAddress: string): boolean => {
+	const validCopyAddresses = [
+		`${dotcopyUsername}@guardian.co.uk`,
+		`${dotcopyUsername}@theguardian.com`,
+	];
+
+	// forAddress seems to (sometimes?) come through wrapped in angle brackets...
+	// trim those off
+	if (forAddress.startsWith('<')) {
+		return validCopyAddresses.includes(
+			forAddress.slice(1, forAddress.length - 1),
+		);
+	}
+	return validCopyAddresses.includes(forAddress);
+};
+
+export async function findVerificationFailures(
+	message: SESMessage,
+): Promise<EmailVerificationResult> {
+	const { receipt } = message;
+
+	const sesVerdictChecks = [
 		{ name: 'spamVerdict', status: receipt.spamVerdict.status },
 		{ name: 'virusVerdict', status: receipt.virusVerdict.status },
 		{ name: 'spfVerdict', status: receipt.spfVerdict.status },
@@ -9,10 +53,61 @@ export function findVerificationFailures(receipt: SESReceipt) {
 		{ name: 'dmarcVerdict', status: receipt.dmarcVerdict.status },
 	];
 
-	const failedChecks = verdictChecks.filter(({ status }) => status !== 'PASS');
+	const sesFailedChecks = sesVerdictChecks.filter(
+		({ status }) => status !== 'PASS',
+	);
+
+	if (sesFailedChecks.length === 0) {
+		console.log(`Email validation for ${message.mail.messageId}: PASS`);
+		return { pass: true, failedChecks: [] };
+	}
+
+	if (
+		sesFailedChecks.length === 1 &&
+		sesFailedChecks[0]?.name === 'spfVerdict'
+	) {
+		const mailObject = await getFromS3({
+			bucketName: EMAIL_BUCKET_NAME,
+			key: message.mail.messageId,
+		});
+		if (mailObject.status === 'failure') {
+			throw new Error(mailObject.reason);
+		}
+
+		const { arc, receivedChain } = await authenticate(mailObject.body, {
+			trustReceived: true,
+		});
+
+		const wasSentToDotCopy = receivedChain.some((chainLink) => {
+			const value = chainLink.for?.value;
+			return value ? validForCopy(value) : false;
+		});
+
+		const arcValid =
+			arc.status.result === 'pass' &&
+			arc.signature.signingDomain === 'google.com' &&
+			arc.signature.status.result === 'pass' &&
+			arc.authenticationResults.mta === 'mx.google.com' &&
+			arc.authenticationResults.arc.result === 'pass' &&
+			arc.authenticationResults.spf.result === 'pass';
+
+		if (!wasSentToDotCopy) {
+			return {
+				pass: false,
+				failedChecks: [{ name: 'sent_to_dotcopy', status: 'FAIL' }],
+			};
+		} else if (!arcValid) {
+			return {
+				pass: false,
+				failedChecks: [{ name: 'newswires_arc_spf_check', status: 'FAIL' }],
+			};
+		} else {
+			return { pass: true, failedChecks: [] };
+		}
+	}
 
 	return {
-		hasFailures: failedChecks.length !== 0,
-		failedChecks,
+		pass: sesFailedChecks.length === 0,
+		failedChecks: sesFailedChecks,
 	};
 }
