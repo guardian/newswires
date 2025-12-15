ebf0baec2205d4bbb33f10fb0a7717073b1624bf
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-12-15 14:45:32 +0000

    clean up shared package exports

--- shared/src/index.ts
+++
@@ -1,9 +0,0 @@
-// export { initialiseDbConnection } from './rds';
-export * from './lambda-logging';
-export * from './constants';
-export * from './precomputeCategories';
-export * from './types';
-export * from './config';
-export * from './findVerificationFailures';
-export * from './rds';
-export * from './s3';
