695511a3a4148542431a1034ad5462f9f69ab5ac
lindseydew <lindsey.dew@guardian.co.uk>
2026-01-07 10:50:29 +0000

    Undo the whitespace change

--- newswires/client/.storybook/preview.ts
+++ newswires/client/.storybook/preview.ts
@@ -2,7 +2,7 @@ import type { Preview } from '@storybook/react-vite';
 
 const preview: Preview = {
 	parameters: {
-			controls: {
+		controls: {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i,
