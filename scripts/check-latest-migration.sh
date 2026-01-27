205312f5360ff120b8623d01ebc5ddbc5bc40e17
Pete F <pete.faulconbridge@theguardian.com>
2026-01-27 12:22:42 +0000

    Fix migration script name and update migration ci check to detect invalid names

--- scripts/check-latest-migration.sh
+++ scripts/check-latest-migration.sh
@@ -8,6 +8,12 @@ echo "Latest migration version in SSM Parameter Store: $VERSION"
 FILES=$(ls db/migrations)
 for file in $FILES; do
    VERSION_NUMBER=$(echo $file | sed -E 's/^V([0-9]+)__.*$/\1/')
+   number_regex='^[0-9]+$'
+   if ! [[ $VERSION_NUMBER =~ $number_regex ]] ; then
+    echo "❌ error: Version number ${VERSION_NUMBER} is not a valid number."
+    echo "💡 Flyway requires migrations to have a filename starting with 'V' followed by a number and then two underscores." >&2;
+    exit 1
+   fi
    if [ "$VERSION_NUMBER" -gt "$VERSION" ]; then
      echo "❌ There are new migrations that have not been applied. Latest migration file is $file"
      exit 1
