5f7a8994487699c4d7ed57faa2d999ac45e53ab8
Andrew Nowak <andrew.nowak@guardian.co.uk>
2026-03-09 14:56:22 +0000

    use upper for index normalisation

--- db/migrations/V26__add_toplevel_sourcefeed_column.sql
+++ db/migrations/V26__add_toplevel_sourcefeed_column.sql
@@ -1,4 +1,4 @@
 ALTER TABLE fingerpost_wire_entry
     ADD gu_source_feed TEXT;
 
-CREATE INDEX gu_source_feed_index ON fingerpost_wire_entry(lower(gu_source_feed));
+CREATE INDEX gu_source_feed_index ON fingerpost_wire_entry(upper(gu_source_feed));
