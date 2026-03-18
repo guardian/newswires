4c913fc2d4a25b00e4d249ccf3c0cc4938450d27
Pete F <pete.faulconbridge@theguardian.com>
2026-03-18 16:53:37 +0000

    Add option to auto-scroll to the top when new stories arrive on tickers
    In response to a user request. We've added a 5 second cooldown after any
    other scroll event, so that if you're trying to navigate the page you
    won't be immediately yanked up to the top.
    
    * If new stories arrive within that 5 second cooldown, the previous
      behaviour will show instead, i.e. the bar at the top which says there
      are new stories will show.
    * For any subsequent stories that load after the cooldown, the
      auto-scroll will be triggered.
    
    Co-authored-by: Andrew Nowak <10963046+andrew-nowak@users.noreply.github.com>

--- newswires/client/src/ScrollToTopButton.tsx
+++ newswires/client/src/ScrollToTopButton.tsx
@@ -1,7 +1,13 @@
 import { EuiButton, EuiButtonEmpty, useEuiTheme } from '@elastic/eui';
 import type { ReactNode, RefObject } from 'react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useSearch } from './context/SearchContext';
+import { useUserSettings } from './context/UserSettingsContext';
+import { useEventCallback } from './useEventCallback';
+
+const offset = 16;
+const scrollThreshold = 100;
+
 /**
  * Floating Scroll-to-Top Button bounded to a scroll container
  */
@@ -16,11 +22,41 @@ export const ScrollToTopButton = ({
 	children: ReactNode | ReactNode[];
 }) => {
 	const { euiTheme } = useEuiTheme();
-	const { unseenWiresFromTopOfList } = useSearch();
+	const { state, config, unseenWiresFromTopOfList } = useSearch();
+	const { queryData } = state;
+	const { enableAutoScroll } = useUserSettings();
+	const userScrollTimerRef = useRef<ReturnType<typeof setTimeout>>(undefined);
 
 	const [visible, setVisible] = useState(false);
-	const offset = 16;
-	const scrollThreshold = 100;
+
+	const isTicker = config.ticker;
+
+	const scrollToTop = useCallback(() => {
+		if (containerRef.current) {
+			containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+		} else {
+			window.scrollTo({ top: 0, behavior: 'smooth' });
+		}
+	}, [containerRef]);
+
+	/**
+	 * Using `useEventCallback` custom hook here allow our effect to get the current
+	 * values of `enableAutoScroll`, `isTicker` and `scrollToTop` without having to add them as dependencies
+	 * and thus avoiding to trigger the effect every time those values change, which would cause unwanted auto-scrolls.
+	 * @todo: once we're on React 19, we should be able to replace this with `useEffectEvent` and avoid the need for a custom hook
+	 * @see: https://react.dev/reference/react/useEffectEvent
+	 */
+	const autoScrollIfEnabled = useEventCallback(() => {
+		if (enableAutoScroll && isTicker && !userScrollTimerRef.current) {
+			scrollToTop();
+		}
+	});
+
+	useEffect(() => {
+		if (queryData?.results.some((r) => r.isFromRefresh)) {
+			autoScrollIfEnabled();
+		}
+	}, [autoScrollIfEnabled, queryData?.results]);
 
 	// Show/hide based on scroll offset
 	useEffect(() => {
@@ -28,6 +64,15 @@ export const ScrollToTopButton = ({
 		const onScroll = () => {
 			const scrollTop = scrollEl?.scrollTop;
 			if (scrollTop !== undefined && scrollTop > scrollThreshold) {
+				if (enableAutoScroll && isTicker) {
+					const oldRefVal = userScrollTimerRef.current;
+					userScrollTimerRef.current = setTimeout(() => {
+						userScrollTimerRef.current = undefined;
+					}, 5000);
+					if (oldRefVal !== undefined) {
+						clearTimeout(oldRefVal);
+					}
+				}
 				setVisible(true);
 			} else {
 				setVisible(false);
@@ -36,15 +81,7 @@ export const ScrollToTopButton = ({
 
 		scrollEl?.addEventListener('scroll', onScroll, { passive: true });
 		return () => scrollEl?.removeEventListener('scroll', onScroll);
-	}, [containerRef]);
-
-	const handleClick = () => {
-		if (containerRef.current) {
-			containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
-		} else {
-			window.scrollTo({ top: 0, behavior: 'smooth' });
-		}
-	};
+	}, [containerRef, enableAutoScroll, isTicker]);
 
 	return (
 		<>
@@ -67,7 +104,7 @@ export const ScrollToTopButton = ({
 							size="xs"
 							iconType="dot"
 							color="accentSecondary"
-							onClick={handleClick}
+							onClick={scrollToTop}
 							css={{
 								width: '100%',
 							}}
@@ -83,7 +120,7 @@ export const ScrollToTopButton = ({
 			{visible && (
 				<EuiButton
 					iconType="arrowUp"
-					onClick={handleClick}
+					onClick={scrollToTop}
 					size="m"
 					color="primary"
 					css={{
