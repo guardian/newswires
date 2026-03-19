c7704668d84ae984a5e297e875b9f445443a1391
Pete F <pete.faulconbridge@theguardian.com>
2026-03-19 14:55:50 +0000

    Rewrite auto-scroll logic for readability and to avoid retriggering the 'new story count' effect unnecessarily, which was leading to miscalculation of new stories

--- newswires/client/src/ScrollToTopButton.tsx
+++ newswires/client/src/ScrollToTopButton.tsx
@@ -1,9 +1,11 @@
 import { EuiButton, EuiButtonEmpty, useEuiTheme } from '@elastic/eui';
 import type { ReactNode, RefObject } from 'react';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useSearch } from './context/SearchContext';
 import { useUserSettings } from './context/UserSettingsContext';
-import { debounce } from './debounce';
+
+const offset = 16;
+
 /**
  * Floating Scroll-to-Top Button bounded to a scroll container
  */
@@ -23,23 +25,20 @@ export const ScrollToTopButton = ({
 	const { state, config } = useSearch();
 	const { queryData } = state;
 	const { enableAutoScroll } = useUserSettings();
+	const userScrollTimerRef = useRef<ReturnType<typeof setTimeout>>(undefined);
 
 	const [incomingStories, setIncomingStories] = useState(0);
 	const [visible, setVisible] = useState(false);
 
-	const offset = 16;
-
-	const [allowProgrammaticScroll, setAllowProgrammaticScroll] = useState(true);
-
-	const disallowProgrammaticScrollTemporarily = () =>
-		setAllowProgrammaticScroll(false);
-
-	const reallowDebounced = useMemo(
-		() => debounce(() => setAllowProgrammaticScroll(true), 5000),
-		[],
-	);
-
 	const isTicker = config.ticker;
+
+	const scrollToTop = useCallback(() => {
+		if (containerRef?.current) {
+			containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+		} else {
+			window.scrollTo({ top: 0, behavior: 'smooth' });
+		}
+	}, [containerRef]);
 
 	// Accumulate counts of newly loaded stories
 	useEffect(() => {
@@ -52,30 +51,24 @@ export const ScrollToTopButton = ({
 			enableAutoScroll &&
 			isTicker &&
 			newCount > 0 &&
-			allowProgrammaticScroll
+			!userScrollTimerRef.current
 		) {
-			if (containerRef?.current) {
-				containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
-			} else {
-				window.scrollTo({ top: 0, behavior: 'smooth' });
-			}
+			scrollToTop();
 		}
-	}, [
-		containerRef,
-		queryData,
-		queryData?.results,
-		allowProgrammaticScroll,
-		enableAutoScroll,
-		isTicker,
-	]);
+	}, [scrollToTop, enableAutoScroll, isTicker, queryData]);
 
 	// Show/hide based on scroll offset
 	useEffect(() => {
 		const scrollEl = containerRef?.current ?? window;
 		const onScroll = () => {
 			if (enableAutoScroll && isTicker) {
-				disallowProgrammaticScrollTemporarily();
-				reallowDebounced();
+				const oldRef = userScrollTimerRef.current;
+				userScrollTimerRef.current = setTimeout(() => {
+					userScrollTimerRef.current = undefined;
+				}, 5000);
+				if (oldRef !== undefined) {
+					clearTimeout(oldRef);
+				}
 			}
 			const scrollTop =
 				scrollEl === window
@@ -91,15 +84,7 @@ export const ScrollToTopButton = ({
 
 		scrollEl.addEventListener('scroll', onScroll, { passive: true });
 		return () => scrollEl.removeEventListener('scroll', onScroll);
-	}, [threshold, containerRef, reallowDebounced, enableAutoScroll, isTicker]);
-
-	const handleClick = () => {
-		if (containerRef?.current) {
-			containerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
-		} else {
-			window.scrollTo({ top: 0, behavior: 'smooth' });
-		}
-	};
+	}, [threshold, containerRef, enableAutoScroll, isTicker]);
 
 	return (
 		<>
@@ -122,7 +107,7 @@ export const ScrollToTopButton = ({
 							size="xs"
 							iconType="dot"
 							color="accentSecondary"
-							onClick={handleClick}
+							onClick={scrollToTop}
 							css={{
 								width: '100%',
 							}}
@@ -138,7 +123,7 @@ export const ScrollToTopButton = ({
 			{visible && (
 				<EuiButton
 					iconType="arrowUp"
-					onClick={handleClick}
+					onClick={scrollToTop}
 					size="m"
 					color="primary"
 					css={{
