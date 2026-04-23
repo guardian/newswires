1cffff8702e870995c679b1110cfc7b8a99ef664
lindseydew <lindsey.dew@guardian.co.uk>
2026-04-23 16:53:03 +0100

    Add randomized inserts

--- ingestion-lambda/localRun.ts
+++ ingestion-lambda/localRun.ts
@@ -4,22 +4,28 @@ import { main } from './src/handler';
 import { createDummyFeedEntry } from 'newswires-shared/localRun/exampleFeed';
 import { fileService } from 'newswires-shared/s3';
 
-const { body, externalId } = createDummyFeedEntry();
-fileService.putToS3({bucketName: '', key: externalId, body: JSON.stringify(body)})
-
 run();
 
 async function run() {
-	const event = {
-		Records: [createSQSRecord({ externalId})]
-	}
-	const response = await main(event)
-	if(response && response.batchItemFailures.length > 0) {
-		console.error(`Error running locally. Make sure you have a local database running by executing: ./scripts/setup-local-db.sh`)
-	}
+	setInterval(async () => {
+		const event = {
+			Records: createRandomDocsAndInsertToInMemoryStore()
+		}
+		const response = await main(event)
+		if(response && response.batchItemFailures.length > 0) {
+			console.error(`Error running locally. Make sure you have a local database running by executing: ./scripts/setup-local-db.sh`)
+		}
+	}, 5000)
+	
 }
-
-function createSQSRecord({externalId}: {externalId: string}): SQSRecord {
+const createRandomDocsAndInsertToInMemoryStore = () => {
+	return [...Array(10).fill(0)].map(_ => {
+		const { body, externalId } = createDummyFeedEntry();
+		fileService.putToS3({bucketName: '', key: externalId, body: JSON.stringify(body)})
+		return createSQSRecord({externalId})
+	})
+}
+const createSQSRecord = ({externalId}: {externalId: string}): SQSRecord => {
 	const randomSqsMessageId = Math.random().toString(36).substring(7);
 	const recordThatShouldSucceed: SQSRecord = {
 		messageId: randomSqsMessageId,
--- shared/src/rds.ts
+++ shared/src/rds.ts
@@ -2,7 +2,7 @@ import { Signer } from '@aws-sdk/rds-signer';
 import postgres from 'postgres';
 import { databaseConfig } from './config';
 
-const { port, hostname, username } = databaseConfig;
+const { port, hostname, username, ssl, name } = databaseConfig;
 
 const signer = new Signer({
 	port,
@@ -14,7 +14,11 @@ export async function initialiseDbConnection() {
 	const token = databaseConfig.password ?? (await signer.getAuthToken());
 
 	const sql = postgres({
-		...databaseConfig,
+		port,
+		hostname,
+		username,
+		ssl,
+		database: name,
 		password: token,
 		idle_timeout: 10,
 		max_lifetime: 60 * 15, // todo -- import from cdk max lambda timeout config?
