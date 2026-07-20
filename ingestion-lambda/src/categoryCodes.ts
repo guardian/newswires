280fbfd864bb07501a0437e4ba8a5babde375714
Pete F <pete.faulconbridge@theguardian.com>
2026-07-20 12:59:51 +0100

    Extract regex into const first

--- ingestion-lambda/src/categoryCodes.ts
+++ ingestion-lambda/src/categoryCodes.ts
@@ -243,6 +243,9 @@ export function inferGBCategoryFromText(content: string | undefined): string[] {
 	); /** @todo we should be able to remove the type predicate after we upgrade TS to 5.6 */
 }
 
+const obituaryPhrasesRexExp =
+	/has died|died at age|died aged|dies at age|dies aged/i;
+
 export function inferObituaryCategoryFromText(
 	content: string | undefined,
 ): string[] {
@@ -250,8 +253,7 @@ export function inferObituaryCategoryFromText(
 		return [];
 	}
 
-	const hasObituaryPhrase =
-		/has died|died at age|died aged|dies at age|dies aged/i.test(content);
+	const hasObituaryPhrase = obituaryPhrasesRexExp.test(content);
 
 	return hasObituaryPhrase ? ['gu:obits'] : [];
 }
